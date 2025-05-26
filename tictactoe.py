a7012cc3ffd77505dd09f83cd58aaa500a2f4f49
CodeByElie <nassarelie98@gmail.com>
2025-05-26 20:41:52 +0200

    Implement isEnded method to check for game completion in TicTacToe class

--- tictactoe.py
+++ tictactoe.py
@@ -37,3 +37,24 @@ class TicTacToe:
             assert(f"Cell at position ({x}, {y}) is already occupied.")
         self.__tab[y][x]=self.__turn
         self.__turn= (self.__turn+1)%2
+    
+    def isEnded(self)  -> bool:
+        """
+        Checks if the Tic-Tac-Toe game has ended.
+        The game is considered ended if any row, column, or diagonal contains the same value 
+        (indicating a win for one of the players).
+        Returns:
+            bool: True if the game has ended (a player has won), False otherwise.
+        """
+
+        for line in self.__tab:
+            if line[0]==line[1] and line[1]==line[2]:
+                return True
+        for i in range(3):
+            if self.__tab[0][i]==self.__tab[1][i] and self.__tab[1][i]==self.__tab[2][i]:
+                return True
+        if self.__tab[0][0]==self.__tab[1][1] and self.__tab[1][1]==self.__tab[2][2]:
+            return True
+        if self.__tab[0][2]==self.__tab[1][1] and self.__tab[1][1]==self.__tab[2][0]:
+            return True
+        return False
